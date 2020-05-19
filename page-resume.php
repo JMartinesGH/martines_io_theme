4db730d7c233e0aa88099b8a2d7232bb0ad2c409
Jamie Martines <dev.jpm@gmail.com>
2020-05-18 20:24:52 -0400

    added temporary resume page template using hard coding before integrating as either ACF fields or gutenberg blocks

---
+++ page-resume.php
@@ -0,0 +1,90 @@
+<?php /* Template Name: Resume Template */ ?>
+<?php get_header(); ?>
+<!-- temporary hard coding -->
+<div class="row">
+  <div class="column square">
+    <h3>Experience</h3>
+    <div class="job">
+      <strong>
+        Front-End Developer <br />
+        One Kings Lane, E-Commerce Team
+      </strong>
+      <br />
+      <span class='small'>
+        NEW YORK, NY || NOV 2017 &dash;
+        JULY
+        2019
+      </span>
+      <ul>
+        <li>Created bespoke marketing materials for the home and landing pages</li>
+        <li>Developed custom widgets in Java for the Kibo environment, Javascript for landing pages, and
+          React</li>
+        <li>Performed A/B Testing using Optimizely</li>
+      </ul>
+    </div>
+    <div class="job">
+      <strong>
+        Developer </strong><br />
+      Wall Street Journal Custom Studios
+      <br />
+      <span class='small'>
+        NEW YORK, NY || OCT 2016 &dash;
+        OCT
+        2017
+      </span>
+      <ul>
+        <li>Development of custom WordPress themes, plugins, and extensions for advertising campaigns
+        </li>
+        <li>Created internal tools for reducing repeated developer and performance tasks</li>
+        <li>Worked alongside editors & project managers to create a better UX for custom WordPress tools
+        </li>
+        <li>Evangelized build tools and templatization to the development team</li>
+        <li>Lead developer on a product built on top of WordPress for client self-publishing with
+          internal reporting, customizable templates, and scalability. Integral to the projects
+          architecture, development cycle, and internal language.</li>
+      </ul>
+    </div>
+    <div class="job">
+      <strong>
+        Developer </strong><br />
+      Reflexions
+      <br />
+      <span class='small'>
+        NEW YORK, NY || SEPT 2015 &dash;
+        OCT
+        2016
+      </span>
+      <ul>
+        <li>Working with designers and clients to develop custom WordPress (themes & plugins) & Laravel
+        </li>
+        <li>Project management and client relationships</li>
+        <li>3D Modeling for a custom WebGL application</li>
+        <li>iOS development with Swift for a prototype application</li>
+      </ul>
+    </div>
+    <div class="job">For full work history see my <a href="https://www.linkedin.com/in/martinesj/" target='_blank'>LinkedIn <span class='print'>https://linkedin.com/in/martinesj/</span></a></div>
+  </div>
+  <div class="column three">
+    <div id="skills" class='square'>
+      <h3>Skills</h3>
+      <p><strong>Primary:</strong> HTML, CSS (Sass, SCSS), JavaScript (Vanilla, JQuery), PHP (WordPress,
+        Laravel) </p>
+      <p><strong>Secondary:</strong> JavaScript (React, Redux, Node.js), Blender, Photoshop, Illustrator
+      </p>
+      <p><strong>Tools:</strong> Puppeteer, Jest, Jasmine</p>
+      <p><strong>Tools:</strong> Git, Yarn, Gulp.js, Bower, NPM</p>
+    </div>
+    <div id="education" class='square'>
+      <h3>Education</h3>
+      <p><strong>Rochester Institute of Technology</strong> &mdash; Rochester, NY</p>
+      <p>B.S. Information Technology
+        <br /><span class='small'>AUG. 2013</span></p>
+    </div>
+    <div id="interests" class='square'>
+      <h3>Hobbies &amp; Interests</h3>
+      <p>3D modeling and game development, collecting pulp science fiction novels & vinyl records,
+        learning guitar, and hanging out with my dog</p>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
