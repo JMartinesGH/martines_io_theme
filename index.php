9172c3aab49331ff687842568ea19c61ddc2838f
Jamie Martines <dev.jpm@gmail.com>
2020-05-19 12:06:20 -0400

    changed the markup on the posts section, added styling for the updated markup including switching h3s to h2s and including a link inside.

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <div class="row">
   <div class="column square">
 
-
+    <h3>Posts</h3>
     <?php
     $query = new WP_Query(array('posts_per_page' => 3));
     if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
-        <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo wp_trim_words( get_the_title(), 10, '...' ); ?></a></h3>
+        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo wp_trim_words( get_the_title(), 10, '...' ); ?></a></h2>
 
         <?php the_excerpt(); ?>
         <?php wp_link_pages(); ?>
