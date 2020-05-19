d789c0c77bdc4fe88a01a7e45e695ea645f55295
Jamie Martines <dev.jpm@gmail.com>
2020-05-19 11:48:29 -0400

    placeholder file for custom post types(projects, jobs?), add filter to replace ellipsis with "Read More", styled read more

--- functions.php
+++ functions.php
@@ -14,10 +14,24 @@ function register_menus() {
 }
 add_action( 'init', 'register_menus' );
 
+// add Read More to excerpts
+function mio_excerpt_more( $more ) {
+  if ( ! is_single() ) {
+      $more = sprintf( '<a class="read-more" href="%1$s">%2$s </a>',
+          get_permalink( get_the_ID() ),
+          __( 'Read More', 'martinesio' )
+      );
+  }
+
+  return $more;
+}
+add_filter( 'excerpt_more', 'mio_excerpt_more' );
+
 // CUSTOMIZER ADD-ONS
 require_once( get_template_directory() . '/functions/customizer.php');
 
 // Custom Post Types
+require_once( get_template_directory() . '/functions/post-types.php');
 
 // acf
 require_once( get_template_directory() . '/functions/acf.php');
