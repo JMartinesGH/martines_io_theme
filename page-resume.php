1abbdf4ee65a9ad09083c4ae50b818e47c8b55e8
Jamie Martines <dev.jpm@gmail.com>
2020-05-19 20:21:45 -0400

    added custom field handlers for the resume page.

--- page-resume.php
+++ page-resume.php
@@ -17,18 +17,18 @@
           </strong>
           <br />
           <span class='small'>
-          <?php the_sub_field('job_location'); ?> || <?php the_sub_field('job_date_range'); ?>
+            <?php the_sub_field('job_location'); ?> || <?php the_sub_field('job_date_range'); ?>
           </span>
           <ul>
-          <?php
-          // check if the repeater field has rows of data
-          if (have_rows('job_about')) :
+            <?php
+            // check if the repeater field has rows of data
+            if (have_rows('job_about')) :
 
-            // loop through the rows of data
-            while (have_rows('job_about')) : the_row(); ?>
-              <li><?php the_sub_field('job_about_task');?></li>
+              // loop through the rows of data
+              while (have_rows('job_about')) : the_row(); ?>
+                <li><?php the_sub_field('job_about_task'); ?></li>
             <?php endwhile;
-          endif; ?>
+            endif; ?>
           </ul>
         </div>
     <?php endwhile;
@@ -41,37 +41,30 @@
 
     ?>
     <div class="job">
-    <?php
-    if (have_posts()) {
+      <?php
+      if (have_posts()) {
 
-      while (have_posts()) {
-        the_post();
+        while (have_posts()) {
+          the_post();
           the_content();
-      }
-    }?>
+        }
+      } ?>
     </div>
   </div>
   <div class="column three">
-    <div id="skills" class='square'>
-      <h3>Skills</h3>
-      <p><strong>Primary:</strong> HTML, CSS (Sass, SCSS), JavaScript (Vanilla, JQuery), PHP (WordPress,
-        Laravel) </p>
-      <p><strong>Secondary:</strong> JavaScript (React, Redux, Node.js), Blender, Photoshop, Illustrator
-      </p>
-      <p><strong>Tools:</strong> Puppeteer, Jest, Jasmine</p>
-      <p><strong>Tools:</strong> Git, Yarn, Gulp.js, Bower, NPM</p>
-    </div>
-    <div id="education" class='square'>
-      <h3>Education</h3>
-      <p><strong>Rochester Institute of Technology</strong> &mdash; Rochester, NY</p>
-      <p>B.S. Information Technology
-        <br /><span class='small'>AUG. 2013</span></p>
-    </div>
-    <div id="interests" class='square'>
-      <h3>Hobbies &amp; Interests</h3>
-      <p>3D modeling and game development, collecting pulp science fiction novels & vinyl records,
-        learning guitar, and hanging out with my dog</p>
-    </div>
+    <?php
+    // check if the repeater field has rows of data
+    if (have_rows('resume-widgets')) :
+
+      // loop through the rows of data
+      while (have_rows('resume-widgets')) : the_row(); ?>
+        <div class='square'>
+          <h3><?php the_sub_field('resume-widget-header'); ?></h3>
+          <?php the_sub_field('resume-widget-body'); ?>
+        </div>
+    <?php endwhile;
+    endif; ?>
+
   </div>
 </div>
 <?php get_footer(); ?>
